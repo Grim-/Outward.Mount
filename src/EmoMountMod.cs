31d3af9cf220c42386f3810517e6fe0988ed3cbe
Bry <pheonix2105@gmail.com>
2022-06-09 23:25:55 +0100

    init

---
+++ src/EmoMountMod.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SideLoader;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EmoMount
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class EmoMountMod : BaseUnityPlugin
+    {
+        // Choose a GUID for your project. Change "myname" and "mymod".
+        public const string GUID = "emo.mountmod";
+        // Choose a NAME for your project, generally the same as your Assembly Name.
+        public const string NAME = "Emo's Mount Mod";
+        // Increment the VERSION when you release a new version of your mod.
+        public const string VERSION = "1.0.0";
+
+        // For accessing your BepInEx Logger from outside of this class (MyMod.Log)
+        internal static ManualLogSource Log;
+
+        // If you need settings, define them like so:
+        public static ConfigEntry<bool> ExampleConfig;
+
+        public const string MOUNT_KEY = "EmoMountToggleKey";
+
+        // Awake is called when your plugin is created. Use this to set up your mod.
+        internal void Awake()
+        {
+            Log = this.Logger;
+
+            InitKeybinds();   
+            //new Harmony(GUID).PatchAll();
+        }
+
+
+        private void InitKeybinds()
+        {
+            CustomKeybindings.AddAction(MOUNT_KEY, KeybindingsCategory.CustomKeybindings);
+        }
+
+
+        // Update is called once per frame. Use this only if needed.
+        // You also have all other MonoBehaviour methods available (OnGUI, etc)
+        internal void Update()
+        {
+
+        }
+    }
+
+    [HarmonyPatch(typeof(Character), nameof(Character.Awake))]
+    public class Character_Awake
+    {
+        [HarmonyPrefix]
+        static void Prefix(Character __instance)
+        {
+            //on awake for whatever character this is add the component
+            __instance.gameObject.AddComponent<CharacterMountController>();
+        }
+    }
+}
---
+++ src/OutwardHelpers.cs
@@ -0,0 +1,53 @@
+﻿using SideLoader;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EmoMount
+{
+    public static class OutwardHelpers
+    {
+        public static string EMISSION_VALUE = "_EmissionColor";
+
+        public static void UpdateWeaponDamage(Weapon WeaponInstance, DamageList newDamageList)
+        {
+            WeaponInstance.Damage.Clear();
+            //just fucken nuke everything 
+            WeaponInstance.Stats.BaseDamage = newDamageList;
+            WeaponInstance.m_baseDamage = WeaponInstance.Stats.BaseDamage.Clone();
+            WeaponInstance.m_activeBaseDamage = WeaponInstance.Stats.BaseDamage.Clone();
+            WeaponInstance.baseDamage = WeaponInstance.Stats.BaseDamage.Clone();
+            WeaponInstance.Stats.Attacks = SL_WeaponStats.GetScaledAttackData(WeaponInstance);
+            //ta-da updated weapon damage
+        }
+
+        public static Renderer TryGetWeaponRenderer(Weapon weaponGameObject)
+        {
+            return weaponGameObject.LoadedVisual.GetComponentInChildren<BoxCollider>().GetComponent<Renderer>();
+        }
+
+        public static T GetFromAssetBundle<T>(string SLPackName, string AssetBundle, string key) where T : UnityEngine.Object
+        {
+            if (!SL.PacksLoaded)
+            {
+                return default(T);
+            }
+
+            return SL.GetSLPack(SLPackName).AssetBundles[AssetBundle].LoadAsset<T>(key);
+        }
+
+
+        public static List<T> GetTypeFromColliders<T>(Collider[] colliders) where T : Component
+        {
+            List<T> list = new List<T>();
+            foreach (var col in colliders)
+            {
+                T type = col.GetComponentInChildren<T>();
+                if (type != null)
+                {
+                    list.Add(type);
+                }
+            }
+            return list;
+        }
+    }
+}
